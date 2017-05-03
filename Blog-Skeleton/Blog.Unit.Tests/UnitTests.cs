854a7e9bb0bae614930b3b5dc577fd0b958e2921
KristinKrastev <kris.krastev90@gmail.com>
2017-05-04 02:26:34 +0300

    Created Page Folders and Additional Project for Tests
    DIfferent Folders for the POM created. Installed the neccessary NuGet
    Packeges to get 2 projects in one solution to run. Tests can run only in
    Firefox. Chrome still working on it.

---
+++ Blog-Skeleton/Blog.Unit.Tests/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Blog.Unit.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void CheckSiteLoad()
+        {
+            IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+            driver.Navigate().GoToUrl("http://localhost:60634/Article/List");
+
+            var logo = wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
+
+            // TODO: Add your test code here
+            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
+        }
+    }
+}
